be00015e99d8841fb307c66338b9355a41db69ae
MicheleMorelli <mmorel05@dcs.bbk.ac.uk>
2018-11-14 00:50:57 +0000

    changed string interpolation style to allow pre-3.6 compatibility

--- inv_index.py
+++ inv_index.py
@@ -11,32 +11,29 @@ def inv_index(d):
             idx[element] = idx.get(element, []) + [k]
     return idx
 
-
 def intersect(d):
     pass
 
-
 def print_d(d):
     for k,v in d.items():
-        print(f"{k} contains {v}.")
+        print("%s contains %s." % (k,v))
 
 def print_inv_d(d):
     for k,v in d.items():
-        print(f"{k} is in {v}.")
+        print("%s is in pages %s." % (k,v))
 
-d = {
-'a' : [1,2],
-'b': [1,2,7,9,7,8,7,7,56,567],
-'c' : [1,2,625,8,4,3,27,7,56,567],
-'d' : [1,2,625,8,4,3,567],
-'e' : [25,53,7,9,567],
-'f' : [56],
-'g' : [53,56,567],
-'h' : [6,567],
-}
+def main():
+    the_internet = {
+    'a' : [1,2],
+    'b': [1,2,7,9,7,8,7,7,56,567],
+    'c' : [1,2,625,8,4,3,27,7,56,567],
+    'd' : [1,2,625,8,4,3,567],
+    'e' : [25,53,7,9,567],
+    'f' : [56],
+    'g' : [53,56,567],
+    'h' : [6,567],
+    }
+    retrieve(the_internet)
 
-#intersect(d)
-
-
-retrieve(d)
-
+if (__name__=='__main__'):
+    main()
